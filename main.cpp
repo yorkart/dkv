7b62dc2ca8fbfcc170162adc86efb6c92e26f732
wangyue1 <wangyue1@ly.com>
2021-09-20 19:15:46 +0800

    grpc

--- main.cpp
+++ main.cpp
@@ -1,8 +1,54 @@
-
 #include <iostream>
-#include "cmake-build-debug/generator/pb_echo.pb.h"
+#include <memory>
+#include <string>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include <grpcpp/grpcpp.h>
+
+#include "helloworld.grpc.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+using helloworld::Greeter;
+using helloworld::HelloReply;
+using helloworld::HelloRequest;
+
+// Logic and data behind the server's behavior.
+class GreeterServiceImpl final : public Greeter::Service {
+    Status SayHello(ServerContext* context, const HelloRequest* request,
+                    HelloReply* reply) override {
+        // Overwrite the call's compression algorithm to DEFLATE.
+        context->set_compression_algorithm(GRPC_COMPRESS_DEFLATE);
+        std::string prefix("Hello ");
+        reply->set_message(prefix + request->name());
+        return Status::OK;
+    }
+};
+
+void RunServer() {
+    std::string server_address("0.0.0.0:50051");
+    GreeterServiceImpl service;
+
+    ServerBuilder builder;
+    // Set the default compression algorithm for the server.
+    builder.SetDefaultCompressionAlgorithm(GRPC_COMPRESS_GZIP);
+    // Listen on the given address without any authentication mechanism.
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    // Register "service" as the instance through which we'll communicate with
+    // clients. In this case it corresponds to an *synchronous* service.
+    builder.RegisterService(&service);
+    // Finally assemble the server.
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
+
+    // Wait for the server to shutdown. Note that some other thread must be
+    // responsible for shutting down the server for this call to ever return.
+    server->Wait();
+}
+
+int main(int argc, char** argv) {
+    RunServer();
+
     return 0;
 }
